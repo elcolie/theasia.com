2393d800bb089bd64dce89e721d0d80bde25de26
Sarit Ritwirune <sarit@elcolie.com>
2018-01-21 15:11:16 +0700

    Move to single compoent. Then I will choose later to refactor or not

--- src/BookingCalculation/Compnents/BookingCalculation.js
+++ src/BookingCalculation/Compnents/BookingCalculation.js
@@ -3,12 +3,19 @@ import {connect} from 'react-redux';
 import {Field, reduxForm} from 'redux-form'
 import DropdownList from 'react-widgets/lib/DropdownList'
 import {FETCH_PRODUCTS} from "../../const";
-import ReactWidgetsForm from "./Form";
+import 'react-widgets/dist/css/react-widgets.css';
+import DateTimePicker from 'react-widgets/lib/DateTimePicker';
+import moment from 'moment';
+import momentLocalizer from "react-widgets-moment";
+
+momentLocalizer(moment);
+
 
 //TODO: Rename it to meet the convention project meaning
 class BookingCalculation extends Component {
   constructor(props) {
     super(props);
+    this.state = { value : '--'};
     this.colors = [{color: 'Red', value: 'ff0000'},
       {color: 'Green', value: '00ff00'},
       {color: 'Blue', value: '0000ff'}];
@@ -21,10 +28,18 @@ class BookingCalculation extends Component {
   render() {
     console.log(this.props.variants);
     return (
-      <ReactWidgetsForm
-        variants={this.props.variants}
-        colors={this.colors}
-      />
+      <Fragment>
+        <DropdownList
+          data={this.props.variants}
+          value={this.state.value}
+          valueField='id' //Will be name later
+          textField='id'
+          onChange={(value) =>{
+            console.log(value);
+            this.setState({value});
+          }}
+        />
+      </Fragment>
     )
   };
 }
